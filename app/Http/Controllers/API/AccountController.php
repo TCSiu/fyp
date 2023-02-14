8473f54623ae9400bd35fc605603fc21244a4333
tcsiu <amble3232@gmail.com>
2023-02-14 19:54:18 +0800

    update 14/2/2023

--- app/Http/Controllers/API/AccountController.php
+++ app/Http/Controllers/API/AccountController.php
@@ -12,58 +12,60 @@ use Validator;
 
 class AccountController extends BaseController
 {
-    public function register(Request $request){
-        $company_info = $request->only(['company_name', 'office_address', 'office_email', 'office_phone', 'warehouse_address1', 'warehouse_address2', 'warehouse_lat', 'warehouse_lng']);
-        $admin_account = $request->only(['username', 'password', 'password_confirmation', 'first_name', 'last_name', 'email', 'phone', 'type', 'company_id']);
-        $company_validator = Validator::make($company_info, [
-            'company_name'          =>  'required|string|max:255|unique:company',
-            'office_address'        =>  'required|string|max:255',
-            'office_email'          =>  'required|string|email|max:255|unique:company',
-            'office_phone'          =>  'required|string|max:20',
-            'warehouse_address1'    =>  'required|string|max:255',
-            'warehouse_address2'    =>  'string|max:255',
-            'warehouse_lat'         =>  'required|integer|max:255',
-            'warehouse_lng'         =>  'required|integer|max:255',
-        ]);
-        $admin_validator = Validator::make($admin_account, [
-            'username'              =>  'required|string|min:5|max:20|unique:account',
-            'password'              =>  'required|confirmed|Regex:/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?\d)[A-Za-z\d!@#$%^\&*()_+-={}\[\]:\";\'<>,\.\\?~`]{8,}$/', 
-            'email'                 =>  'required|string|email|max:255|unique:account',
-            'phone'                 =>  'required|string|max:20',
-            'first_name'            =>  'string|max:255',
-            'last_name'             =>  'string|max:255',
-        ]);
-        if($company_validator->fails()){
-            return $this->sendError('Validation Error', $company_validator->errors());
-        }
-        if($admin_validator->fails()){
-            return $this->sendError('Validation Error', $admin_validator->errors());
-        }
-        $company = Company::create($company_info);
-        $admin_account['password'] = password_hash($admin_account['password'], PASSWORD_DEFAULT);
-        $admin_account['type'] = 'admin';
-        $admin_account['company_id'] = $company->id;
-        $account = Account::create($admin_account);
-        $success['token'] = $account->createToken('FYP')->accessToken;
-        $success['account'] = $account->username;
-        return $this->sendResponse($success, 'User Register Success!');
-    }
+    // public function register(Request $request){
+    //     $company_info = $request->only(['company_name', 'office_address', 'office_email', 'office_phone', 'warehouse_address1', 'warehouse_address2', 'warehouse_lat', 'warehouse_lng']);
+    //     $admin_account = $request->only(['username', 'password', 'password_confirmation', 'first_name', 'last_name', 'email', 'phone', 'type', 'company_id']);
+    //     $company_validator = Validator::make($company_info, [
+    //         'company_name'          =>  'required|string|max:255|unique:company',
+    //         'office_address'        =>  'required|string|max:255',
+    //         'office_email'          =>  'required|string|email|max:255|unique:company',
+    //         'office_phone'          =>  'required|string|max:20',
+    //         'warehouse_address1'    =>  'required|string|max:255',
+    //         'warehouse_address2'    =>  'string|max:255',
+    //         'warehouse_lat'         =>  'required|integer|max:255',
+    //         'warehouse_lng'         =>  'required|integer|max:255',
+    //     ]);
+    //     $admin_validator = Validator::make($admin_account, [
+    //         'username'              =>  'required|string|min:5|max:20|unique:account',
+    //         'password'              =>  'required|confirmed|Regex:/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?\d)[A-Za-z\d!@#$%^\&*()_+-={}\[\]:\";\'<>,\.\\?~`]{8,}$/', 
+    //         'email'                 =>  'required|string|email|max:255|unique:account',
+    //         'phone'                 =>  'required|string|max:20',
+    //         'first_name'            =>  'string|max:255',
+    //         'last_name'             =>  'string|max:255',
+    //     ]);
+    //     if($company_validator->fails()){
+    //         return $this->sendError('Validation Error', $company_validator->errors());
+    //     }
+    //     if($admin_validator->fails()){
+    //         return $this->sendError('Validation Error', $admin_validator->errors());
+    //     }
+    //     $company = Company::create($company_info);
+    //     $admin_account['password'] = password_hash($admin_account['password'], PASSWORD_DEFAULT);
+    //     $admin_account['type'] = 'admin';
+    //     $admin_account['company_id'] = $company->id;
+    //     $account = Account::create($admin_account);
+    //     $success['token'] = $account->createToken('FYP')->accessToken;
+    //     $success['account'] = $account->username;
+    //     return $this->sendResponse($success, 'User Register Success!');
+    // }
 
     public function login(Request $request){
         if(Auth::attempt(['username'   =>  $request->username, 'password'   =>  $request->password])){
-            $account = Auth::user();
-            $success['token'] = $account->createToken('FYP')->accessToken;
-            $success['account'] = $account;
-            return $this->sendResponse($success, 'User Login Success!');
-        }else{
-            return $this->sendError('Unauthorised!', ['error'=>'Unauthorised!']);
+            $account = $request->user();
+            if(in_array($account->type, ['staff']) > 0){
+                $account = Auth::user();
+                $success['token'] = $account->createToken('FYP')->accessToken;
+                $success['account'] = $account;
+                return $this->sendResponse($success, 'User Login Success!');
+            }
         }
+        return $this->sendError('Unauthorised!', ['error'=>'Unauthorised!']);
     }
 
     public function index(Request $request){
         $user = request()->user();
         $id = $user->id;
         $allTask = Group::findRecord($id);
-        return $this->sendResponse('success', $allTask);
+        return $this->sendResponse($allTask, 'success');
     }
 }
